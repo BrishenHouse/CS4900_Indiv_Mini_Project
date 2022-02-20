5e36af59ab8748651620a73fb8865bc39255e545
Brishen House <75764264+BrishenHouse@users.noreply.github.com>
2022-02-20 17:47:23 -0500

    Updates to MainActivity and activity_main.xml, and upload of quantized resnet18 script

--- MainActivity.java
+++ MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.house_indiv_mini_project;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -18,28 +21,39 @@ import org.pytorch.Module;
 import org.pytorch.Tensor;
 import org.pytorch.torchvision.TensorImageUtils;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity{
-    Bitmap bitmap;
-    Uri selectedImage;
-    ImageView imageView;
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        Button gallery = findViewById(R.id.gallery);
-        gallery.setOnClickListener(new View.OnClickListener() {
+    private Bitmap bitmap = null;
+    private Module module = null;
+    private ImageView imageView = null;
+    private Uri selectedImage = null;
 
 
-            public void onClick(View view){
-                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                //Intent.setType("image/*");
-                startActivityForResult(intent, 3);
+    // Returns the string of the absolute path of the assets
+    public static String assetFilePath(Context context, String assetName) throws IOException{
+        File file = new File(context.getFilesDir(), assetName);
+        if( file.exists() && file.length() > 0){
+            return file.getAbsolutePath();
+        }
+
+        try (InputStream is = context.getAssets().open(assetName)){
+            try(OutputStream os = new FileOutputStream(file)){
+                byte[] buffer = new byte[4 * 1024];
+                int read;
+                while((read = is.read(buffer)) != -1){
+                    os.write(buffer, 0, read);
+                }
+                os.flush();
             }
-        });
+            return file.getAbsolutePath();
+        }
     }
 
     @Override
@@ -48,13 +62,13 @@ public class MainActivity extends AppCompatActivity{
 
         if (resultCode == RESULT_OK && data != null) {
 
-            ImageView imageView = findViewById(R.id.imageView);
+            imageView = findViewById(R.id.imageView);
             try {
                 //receive the data from selected image
                 InputStream inputStream = getContentResolver().openInputStream(data.getData());
-                //create a bitmap of that image
-                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-                //set the imageView bitmap to the selected image bitmap
+                //create a bitmap of that image and set it to the Bitmap variable we declared earlier
+                bitmap = BitmapFactory.decodeStream(inputStream);
+                //set the imageView bitmap to the selected image bitmap (possibly not needed)
                 imageView.setImageBitmap(bitmap);
             }
             catch (FileNotFoundException e) {
@@ -63,45 +77,80 @@ public class MainActivity extends AppCompatActivity{
         }
     }
 
-    public static Bitmap invertImage(Bitmap original){
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        Bitmap finalimage = Bitmap.createBitmap(original.getWidth(), original.getHeight(), original.getConfig());
-
-        int A;
-        int R;
-        int G;
-        int B;
-        int pixelColor;
-        int height = original.getHeight();
-        int width = original.getWidth();
-
-        for (int y = 0; y < height; y++)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                pixelColor = original.getPixel(x, y);
-                A = Color.alpha(pixelColor);
-                R = 255 - Color.red(pixelColor);
-                G = 255 - Color.green(pixelColor);
-                B = 255 - Color.blue(pixelColor);
-                finalimage.setPixel(x, y, Color.argb(A, R, G, B));
-            }
+        // Read External Storage (Possibly not needed, also permission in "AndroidManifest.xml")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]
+                    {android.Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
         }
-        return finalimage;
-    }
 
-    public void classify(View view) throws IOException {
-        // Gets Bitmap of loaded image
-        imageView = findViewById(R.id.imageView);
-        bitmap = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), selectedImage);
+        try {
+            // (possibly not needed)
+            //bitmap = BitmapFactory.decodeStream(getAssets().open("kitten.jpg"));
 
-        Module module = Module.load("vgg16_model_scripted.py");
-        Tensor inputTensor = TensorImageUtils.bitmapToFloat32Tensor(bitmap,
-                TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB);
-        Tensor outputTensor = module.forward(IValue.from(inputTensor)).toTensor();
-        float[] scores = outputTensor.getDataAsFloatArray();
+            //loading serialized torchscript module from package into app android asset resnet18_scripted.py
+            //app/src/module/assets/resnet18_scripted.py
+            module = Module.load(assetFilePath(this, "resnet18_scripted.py"));
 
-        Bitmap newImage = invertImage(bitmap);
-        imageView.setImageBitmap(newImage);
+        } catch (IOException e) {
+            Log.e("PTRDryRun", "Error Reading assets", e);
+            finish();
+        }
+
+        // (possibly not needed) showing Image on UI
+        //ImageView imageView = findViewById(R.id.imageView);
+        //imageView.setImageBitmap(bitmap);
+
+        // determining the functionality of the gallery button
+        Button gallery = findViewById(R.id.gallery);
+        gallery.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 3);
+            }
+
+        });
+
+        // determining the functionality of the classify button
+        Button classify = findViewById(R.id.classify);
+        classify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//                //Intent.setType("image/*");
+//                startActivityForResult(intent, 3);
+
+                final Tensor inputTensor = TensorImageUtils.bitmapToFloat32Tensor(bitmap,
+                        TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB);
+
+                // running the model
+                final Tensor outputTensor = module.forward(IValue.from(inputTensor)).toTensor();
+
+                // converting Tensor content as java float array
+                final float[] scores = outputTensor.getDataAsFloatArray();
+
+                //searching for the index with max score
+                float maxScore = -Float.MAX_VALUE;
+                int maxScoreIdx = -1;
+                for(int i=0; i<scores.length; i++){
+                    if(scores[i] > maxScore){
+                        maxScore = scores[i];
+                        maxScoreIdx = i;
+                    }
+                }
+
+                //String className = com.example.classifier.ImageNetClasses.IMAGENET_CLASSES[maxScoreIdx];
+
+                // showing classname on UI
+                //TextView textView = findViewById(R.id.resultView);
+                //textView.setText(className);
+            }
+        });
     }
 }
